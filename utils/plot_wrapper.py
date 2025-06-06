4acbf04e753b4417dc6b6b4da66bfc2f4df7590b
= <=>
2025-06-06 13:21:38 +0530

    Data Augmentation

---
+++ utils/plot_wrapper.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+def plot_image(image, title=None, cmap=None):
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image, cmap=cmap)
+    if title:
+        plt.title(title)
+    plt.axis('off')
+    plt.show()
+
+def plot_images(images, titles=None, cmap=None, cols=2):
+    n = len(images)
+    nrows = (n + cols - 1) // cols
+    plt.figure(figsize=(10 * cols, 10 * nrows))
+    for i in range(n):
+        plt.subplot(nrows, cols, i + 1)
+        plt.imshow(images[i], cmap=cmap)
+        if titles:
+            plt.title(titles[i])
+        plt.axis('off')
+    plt.show()
